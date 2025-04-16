53d33d32b9a4e4610f2a61f17eb257b55439581e
ltw313 <ltw5208@naver.com>
2025-04-16 12:28:49 +0900

    linux

--- week7/quiz3-10.sh
+++ week7/quiz3-10.sh
@@ -3,10 +3,10 @@ file_count=0
 dir_count=0
 for item in *
 do
-	if [ -d "$item"]
+	if [ -d "$item" ]
 	then 
 	           dir_count=$((dir_count+1))
-        elif [-f "$item"]
+        elif [ -f "$item" ]
         then 
                    file_count=$((file_count+1))
         else
@@ -14,5 +14,5 @@ do
         fi
 done
 echo "파일 수 : $file_count"
-echo "디렉토리 수  $dir_count"
+echo "디렉토리 수 : $dir_count"
 exit 0
